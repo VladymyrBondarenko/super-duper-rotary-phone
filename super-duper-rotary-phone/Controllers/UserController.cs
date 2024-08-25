1da57f553d38eebd9e9e7646a40f21235bb6d5ad
vladymyr.bondarenko1 <vladymyr.bondarenko1@gmail.com>
2024-08-25 12:59:21 +0300

    Add user controller

---
+++ super-duper-rotary-phone/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace super_duper_rotary_phone.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+            
+        }
+
+        public IActionResult Get()
+        {
+            return Ok("Result");
+        }
+    }
+}
