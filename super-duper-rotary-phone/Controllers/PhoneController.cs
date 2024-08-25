f90ad09e4c14955dbd32cc815769217e8b94b67a
vladymyr.bondarenko1 <vladymyr.bondarenko1@gmail.com>
2024-08-25 12:45:25 +0300

    Merge resolving 2

---
+++ super-duper-rotary-phone/Controllers/PhoneController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace super_duper_rotary_phone.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PhoneController : ControllerBase
+    {
+        public PhoneController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Result");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return CreatedAtAction("Post", "Result");
+        }
+    }
+}
